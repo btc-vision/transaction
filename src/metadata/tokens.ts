e0e79aabde0473e47e1a28d6ca676bf5818ddb5a
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-07-22 01:10:12 -0400

    updated contracts

--- src/metadata/tokens.ts
+++ src/metadata/tokens.ts
@@ -3,5 +3,5 @@ import { Address } from '@btc-vision/bsi-binary';
 export const FACTORY_ADDRESS: Address = 'bcrt1qz98txrd6csz0ljhp0v2rw8846k0zgn57uul8c9';
 export const POOL_ADDRESS: Address = 'bcrt1qzwth3t3wdgy83tt9xegq7yww5dx0rhy3ezglg9';
 export const WBTC_ADDRESS: Address = 'bcrt1q99qtptumw027cw8w274tqzd564q66u537vn0lh';
-export const MOTO_ADDRESS: Address = 'bcrt1q60z7qk5700et2qlkqqqqwnmn6zlc7s2dqcwav4';
+export const MOTO_ADDRESS: Address = 'bcrt1qdpw568rsjpfgpx4lm8tdmmpcjfadwjqhvwyzn3';
 export const ROUTER_ADDRESS: Address = 'bcrt1qelqwcguvwkgr90w6u5f2q0a7gqlnq4w8rl26ht';
