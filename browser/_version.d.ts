40aaa684f51e1272beea9b1ba06682d2b35cce44
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-07-28 22:06:52 -0400

    fixed bug

--- browser/_version.d.ts
+++ browser/_version.d.ts
@@ -1,1 +1,1 @@
-export declare const version = "1.0.58";
+export declare const version = "1.0.59";
--- src/transaction/TransactionFactory.ts
+++ src/transaction/TransactionFactory.ts
@@ -170,14 +170,21 @@ export class TransactionFactory {
         // We have to regenerate using the new utxo
         const outTx: Transaction = await finalTransaction.signTransaction();
 
+        const refundUTXO: UTXO = {
+            transactionId: signedTransaction.getId(),
+            outputIndex: 1, // always 1
+            scriptPubKey: {
+                hex: out.script.toString('hex'),
+                address: deploymentParameters.from,
+            },
+            value: BigInt(signedTransaction.outs[1].value),
+        };
+
         return {
             transaction: [signedTransaction.toHex(), outTx.toHex()],
             contractAddress: finalTransaction.contractAddress,
             p2trAddress: finalTransaction.p2trAddress,
-            utxos: [{
-                ...newUtxo,
-                outputIndex: 1, // always 1
-            }],
+            utxos: [refundUTXO],
         };
     }
 
