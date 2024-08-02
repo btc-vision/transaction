70306609ebf002af20fd270ecea729d37589a89c
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-08-02 00:01:13 -0400

    updated wbtc

--- src/metadata/tokens.ts
+++ src/metadata/tokens.ts
@@ -3,13 +3,13 @@ import { Address } from '@btc-vision/bsi-binary';
 // Addresses Regtest
 export const FACTORY_ADDRESS_REGTEST: Address = 'bcrt1qgph2k9dahptnz2vu72ezd9r2p4phjm2pkgu7ht';
 export const POOL_ADDRESS_REGTEST: Address = 'bcrt1q5nkjpjmh6xwweprcyeylwdw7aud4een7p9pphd';
-export const WBTC_ADDRESS_REGTEST: Address = 'bcrt1qnra3nd3gng0u7rqj9cpepsg3vfetldc9phvsrd';
+export const WBTC_ADDRESS_REGTEST: Address = 'bcrt1q9vs429jazf5m7xmkzcu09x3ksg7tlgcevcyr4e';
 export const MOTO_ADDRESS_REGTEST: Address = 'bcrt1q8reuxx9naek4mqesrfsgdpjv3q7a5g2llkh6ua';
 export const ROUTER_ADDRESS_REGTEST: Address = 'bcrt1qyx492l440f6cm5hdppw5pmkd2fc7k3qayuuvh6';
 
 // Addresses Testnet
 export const FACTORY_ADDRESS_TESTNET: Address = 'tb1qgev5kldhp5zvg6j8t9vl6x4phkrwn8nk9felxh';
 export const POOL_ADDRESS_TESTNET: Address = 'tb1q6a7yw353hjmresphupytw5vczpqxtg4yrupayk';
-export const WBTC_ADDRESS_TESTNET: Address = 'tb1qq89wm9vfzmxla7jlstyz8s3fm40vl589r6mltj';
+export const WBTC_ADDRESS_TESTNET: Address = 'tb1q6v6kqjsm7e0whyu2ulx5w6vtp8heyvhgsvfd0w';
 export const MOTO_ADDRESS_TESTNET: Address = 'tb1q4tyhf8hpu04qjj3qaag20knun0spctultxzakw';
 export const ROUTER_ADDRESS_TESTNET: Address = 'tb1qre7tlyrxrs3lht6ls6y2wezgq8hlmr78y2j9y2';
