15c70aff928ae134d99f7575e3f114d624f58660
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-09-15 20:38:32 -0400

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+// @ts-check
+
+import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js';
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.strictTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigDirName: import.meta.dirname,
+            },
+        },
+        rules: {
+            'no-undef': 'off',
+            '@typescript-eslint/no-unused-vars': 'warn',
+            'no-empty': 'off',
+            '@typescript-eslint/restrict-template-expressions': 'off',
+            '@typescript-eslint/only-throw-error': 'off',
+            '@typescript-eslint/no-unnecessary-condition': 'off',
+            '@typescript-eslint/unbound-method': 'warn',
+        },
+    },
+    {
+        files: ['**/*.js'],
+        ...tseslint.configs.disableTypeChecked,
+    },
+);
