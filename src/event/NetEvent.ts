c465d3f54bd8560e1fe9f01ebd4607b11401c7da
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-10-19 04:07:07 -0400

    Update NetEvent.ts

--- src/event/NetEvent.ts
+++ src/event/NetEvent.ts
@@ -1,7 +1,6 @@
 export class NetEvent {
     public constructor(
         public readonly eventType: string,
-        public readonly eventDataSelector: bigint,
         public readonly eventData: Uint8Array,
     ) {}
 }
