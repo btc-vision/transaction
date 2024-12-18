962d7bfda0bcfef7765d4aefdf9c5821ae8c66b6
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-12-18 02:26:41 -0500

    Update BinaryReader.ts

--- src/buffer/BinaryReader.ts
+++ src/buffer/BinaryReader.ts
@@ -165,7 +165,10 @@ export class BinaryReader {
     public readU8(): u8 {
         this.verifyEnd(this.currentOffset + U8_BYTE_LENGTH);
 
-        return this.buffer.getUint8(this.currentOffset + U8_BYTE_LENGTH);
+        const value = this.buffer.getUint8(this.currentOffset);
+        this.currentOffset += U8_BYTE_LENGTH;
+
+        return value;
     }
 
     public readU16(): u16 {
