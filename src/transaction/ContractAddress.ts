59edbff6c5d8ca631db643bb31dc52f089976f13
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-01-01 00:31:23 -0500

    Fixed mistake

--- src/transaction/ContractAddress.ts
+++ src/transaction/ContractAddress.ts
@@ -4,7 +4,7 @@ class ContractAddressBase {
     private readonly deriver: Secp256k1PointDeriver = new Secp256k1PointDeriver();
 
     public generateHybridKeyFromHash(input: Buffer): Buffer {
-        const p = this.deriver.findOrDeriveValidPoint(input, false);
+        const p = this.deriver.findOrDeriveValidPoint(Buffer.copyBytesFrom(input), false);
         const y = this.deriver.getCanonicalY(p.y1, p.y2);
         return Buffer.from(this.deriver.getHybridPublicKey(p.x, y));
     }
