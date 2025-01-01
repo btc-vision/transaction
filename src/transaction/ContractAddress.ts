7eec8b04acb767c9bc2323418ad45355195cddef
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-01-01 00:55:09 -0500

    Dumb broken error

--- src/transaction/ContractAddress.ts
+++ src/transaction/ContractAddress.ts
@@ -4,9 +4,13 @@ class ContractAddressBase {
     private readonly deriver: Secp256k1PointDeriver = new Secp256k1PointDeriver();
 
     public generateHybridKeyFromHash(input: Buffer): Buffer {
-        const p = this.deriver.findOrDeriveValidPoint(Buffer.copyBytesFrom(input), false);
+        const p = this.deriver.findOrDeriveValidPoint(this.cloneBuffer(input), false);
         const y = this.deriver.getCanonicalY(p.y1, p.y2);
         return Buffer.from(this.deriver.getHybridPublicKey(p.x, y));
+    }
+
+    private cloneBuffer(buffer: Buffer): Buffer {
+        return Buffer.from(buffer);
     }
 }
 
