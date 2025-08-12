8ebed563041716e43bd3fac1890d1ba0a9e76738
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-08-12 00:25:21 -0400

    Fixed ts issue & Upgraded deps

--- src/transaction/builders/InteractionTransactionP2WDA.ts
+++ src/transaction/builders/InteractionTransactionP2WDA.ts
@@ -2,7 +2,11 @@ import { Buffer } from 'buffer';
 import { Psbt, PsbtInput, toXOnly } from '@btc-vision/bitcoin';
 import { TransactionType } from '../enums/TransactionType.js';
 import { IInteractionParameters } from '../interfaces/ITransactionParameters.js';
-import { MINIMUM_AMOUNT_CA, MINIMUM_AMOUNT_REWARD, TransactionBuilder, } from './TransactionBuilder.js';
+import {
+    MINIMUM_AMOUNT_CA,
+    MINIMUM_AMOUNT_REWARD,
+    TransactionBuilder,
+} from './TransactionBuilder.js';
 import { MessageSigner } from '../../keypair/MessageSigner.js';
 import { Compressor } from '../../bytecode/Compressor.js';
 import { P2WDAGenerator } from '../../generators/builders/P2WDAGenerator.js';
